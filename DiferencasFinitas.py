557258841cd6c554f660f833e26d8ddf58a0d523
flavio <flavio.00000844530@unicap.br>
2023-11-17 10:24:40 -0300

    implementando diferencas finitas

--- DiferencasFinitas.py
+++ DiferencasFinitas.py
@@ -1,2 +1,52 @@
-def getP(x):
-    print("ok")
+import math
+
+
+def getOrdem(lx, ly):
+    if len(lx) != len(ly):
+        raise Exception("Tamanho diferente")
+    
+    matriz = []
+
+    listToMatriz = []
+    for i in range(len(lx)):
+        if i == 0:
+            for a in range(len(lx)):
+                listToMatriz.append(ly[a])
+        else:
+            for a in range(len(lx) - i):
+                listToMatriz.append(matriz[i-1][a+1] - matriz[i-1][a])
+        matriz.append(listToMatriz)
+        listToMatriz = []
+    
+    return matriz
+
+
+def getP(x, lx, ly):
+    #incompleto
+    if len(lx) != len(ly):
+        raise Exception("Tamanho diferente")
+
+    matriz = getOrdem(lx, ly)
+    h = lx[1] - lx[0]
+    z = (x - lx[0]) / h
+    listResult = []
+
+    for i in range(len(lx)):
+        if i == 0:
+            listResult.append(matriz[i][0])
+            continue
+        else:
+            toAppend = (z / math.factorial(i)) * matriz[i][0]
+            listResult.append(toAppend)
+
+
+    return somaVet(listResult)
+
+
+def somaVet(vet):
+    result = 0
+
+    for i in vet:
+        result += i
+
+    return result
--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 import DiferencasDivididas as DD
 import Lagrange as L
+import DiferencasFinitas as DF
 
 
 
 def main():
-    print(L.getP(0.8, [0.0, 0.5, 1.0], [1.0, 0.6875, 0.0]))
-    print(L.getErroTrunc(0.8, [0.0, 0.5, 1.0]))
-
+    print(DF.getOrdem([1.2, 1.4, 1.6, 1.8], [1.340, 1.678, 1.876, 2.242]))
+    print(DF.getP(1.0, [1.2, 1.4, 1.6, 1.8], [1.340, 1.678, 1.876, 2.242]))
 
 
 
