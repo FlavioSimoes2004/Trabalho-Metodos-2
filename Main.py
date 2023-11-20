1729bf941fade92bfff949d1909e3b4d4fd76043
flavio <flavio.00000844530@unicap.br>
2023-11-20 10:42:09 -0300

    feito

--- Main.py
+++ Main.py
@@ -5,8 +5,31 @@ import DiferencasFinitas as DF
 
 
 def main():
-    #print(DF.getOrdem([1.2, 1.4, 1.6, 1.8], [1.340, 1.678, 1.876, 2.242]))
-    print(DF.getP(1.0, [-1.0, 0.0, 1.0], [1.0, 1.5, 2.5]))
+    x = 1
+    lx = []
+    ly = []
+
+    txt = '0 - fecha programa\n1 - lagrange\n2 - diferencas divididas\n3 - diferencas finitas'
+    choice = 0
+    while True:
+        print(txt)
+        choice = input()
+        while choice < 0 or choice > 3:
+            print('escolha invalida, digite novamente: ')
+            choice = input()
+
+        if choice == 0:
+            break
+        elif choice == 1:
+            print("P = ", L.getP(x, lx, ly))
+            print("erro truncamento = ", L.getErroTrunc(x, lx))
+        elif choice == 2:
+            print("P = ", DD.getP(x, lx, ly))
+            print("erro truncamento = ", DD.getErroTrunc(x, lx))
+        else:
+            print("P = ", DF.getP(x, lx, ly))
+            print("erro truncamento = ", DF.getEt(x, lx))
+        
 
 
 
